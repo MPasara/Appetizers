d36a0b8d245cdc2fa28d56f820cd53d55cabe75d
Mladen <pasaramladen14@gmail.com>
2025-06-24 12:55:32 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Mladen Pašara on 24.06.2025..
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppetizersTests/AppetizersTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersTests.swift
+//  AppetizersTests
+//
+//  Created by Mladen Pašara on 24.06.2025..
+//
+
+import Testing
+@testable import Appetizers
+
+struct AppetizersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
